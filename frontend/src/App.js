a3945031c68d6ffbb3e4b3eee4d380a5d65fa4ce
Drake Olejniczak <drake.olejniczak@gmail.com>
2024-05-12 18:39:55 -0400

    fixed some issues with the server management

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to the Network Visualizer</h1>
+      <p>This is the main component of the application.</p>
+    </div>
+  );
+}
+
+export default App;
