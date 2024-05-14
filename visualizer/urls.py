e0cb9b7fbcf8b6ab02162de5e35463339a023089
Drake Olejniczak <drake.olejniczak@gmail.com>
2024-05-14 01:17:13 -0400

    fixed the api connection issue: had extra 'api/' at the front of all the paths in urls.py

--- visualizer/urls.py
+++ visualizer/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import NetworkData, ComponentsSummary, CharacterDetailView, GuestDetailView, TestData
+from .views import NetworkData, ComponentsSummary, CharacterDetailView, GuestDetailView
 
 urlpatterns = [
-    path('api/network/<str:entity_type>/', NetworkData.as_view(), name='network-data'),
-    path('api/components-summary/<str:entity_type>/', ComponentsSummary.as_view(), name='components_summary'),
-    path('api/characters/<int:character_id>/', CharacterDetailView.as_view(), name='character-detail'),
-    path('api/guests/<int:guest_id>/', GuestDetailView.as_view(), name='guest-detail'),
-    path('api/test/', TestData.as_view(), name='test-data'),
+    path('network/<str:entity_type>/', NetworkData.as_view(), name='network-data'),
+    path('components-summary/<str:entity_type>/', ComponentsSummary.as_view(), name='components_summary'),
+    path('characters/<int:character_id>/', CharacterDetailView.as_view(), name='character-detail'),
+    path('guests/<int:guest_id>/', GuestDetailView.as_view(), name='guest-detail'),
 ]
