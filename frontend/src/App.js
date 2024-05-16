230b56b86b412a8ee90d64201912ec2574eb07d2
Drake Olejniczak <drake.olejniczak@gmail.com>
2024-05-15 23:38:27 -0400

    Fix network display issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Visualizer from './components/Visualizer';
 import Sidebar from './components/Sidebar';
 import Controls from './components/Controls';
+import './styles.css';
 
 const App = () => {
     return (
