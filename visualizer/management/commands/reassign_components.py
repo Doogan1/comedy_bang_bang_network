10d6b13db0cef5ddc987e9f021a261adada23855
Drake Olejniczak <drake.olejniczak@gmail.com>
2024-06-11 10:21:14 -0400

    feat: Update component IDs and associated model references for Character and Guest
    - Added a management command to reassign component IDs in JSON files based on the current order in the file.
    - Updated the `Component` model instances to reflect new IDs.
    - Ensured the `Character` and `Guest` models' component associations are correctly updated based on the new component IDs.
    - Improved the reassign_component_ids function to create a new component ID mapping and save the updated JSON files.
    - Enhanced the handle function to update both JSON files and the corresponding models in the database.
    
    This update resolves issues with character links in the frontend, ensuring they now work as expected.

--- visualizer/management/commands/reassign_components.py
+++ visualizer/management/commands/reassign_components.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from visualizer.models import Character, Guest, CharacterComponent, GuestComponent
 import json
 import os
 from django.conf import settings
@@ -23,6 +24,9 @@ class Command(BaseCommand):
             with open(output_path, 'w') as f:
                 json.dump(data, f, indent=2)
         
+            # Return component_mapping to use on models
+
+            return component_mapping
         # Paths to input and output files
         characters_file = os.path.join(settings.NETWORK_DATA_DIR, f'characters_components.json')
         guests_file = os.path.join(settings.NETWORK_DATA_DIR, f'guests_components.json')
@@ -30,8 +34,44 @@ class Command(BaseCommand):
         updated_guests_file = os.path.join(settings.NETWORK_DATA_DIR, f'guests_components_updated.json')
         
         # Reassign component_ids for both JSON files
-        reassign_component_ids(characters_file, updated_characters_file)
-        reassign_component_ids(guests_file, updated_guests_file)
+        characters_component_mapping = reassign_component_ids(characters_file, updated_characters_file)
+        guests_component_mapping = reassign_component_ids(guests_file, updated_guests_file)
+
+        # Update character components
+        for old_id, new_id in characters_component_mapping.items():
+            component = CharacterComponent.objects.get(pk=old_id)
+            component.pk = new_id
+            component.save()
         
-        self.stdout.write(self.style.SUCCESS('Successfully updated component_ids in both JSON files'))
+        #Update guest components
+        for old_id, new_id in guests_component_mapping.items():
+            component = GuestComponent.objects.get(pk=old_id)
+            component.pk = new_id
+            component.save()
+
+        # Update Character models with mapping
+        characters = Character.objects.all()
+        for character in characters:
+            print(f"Updating {character.name}'s component id, which is currently {character.component.pk}")
+            old_component_id = character.component.pk
+            new_component_id = characters_component_mapping.get(old_component_id)
+            if new_component_id is not None:
+                new_component = CharacterComponent.objects.get(pk=new_component_id)
+                character.component = new_component
+                character.save()
+            print(f"Updated to {character}.  New component_id is {character.component.pk}")
+        #Update Guest models with mapping
+        guests = Guest.objects.all()
+        for guest in guests:
+            print(f"Updating {guest.name}'s component id, which is currently {guest.component.pk}")
+            old_component_id = guest.component.pk
+            new_component_id = guests_component_mapping.get(old_component_id)
+            print(f"Old component id is {old_component_id} and new component id is {new_component_id}")
+            if new_component_id is not None:
+                new_component = GuestComponent.objects.get(pk=new_component_id)
+                guest.component = new_component
+                guest.save()
+            print(f"Updated to {guest}.  New component_id is {guest.component.pk}")
+            
+        self.stdout.write(self.style.SUCCESS('Successfully updated component_ids in both JSON files, the character models, and the guest models'))
 
