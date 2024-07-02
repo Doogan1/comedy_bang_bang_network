bcb6dc4c53d7fcb3e4e5d641274667ee8fc63613
Drake Olejniczak <drake.olejniczak@gmail.com>
2024-07-01 20:15:28 -0400

    Fix network data and recompute database/models

--- frontend/src/features/ui/uiSlice.js
+++ frontend/src/features/ui/uiSlice.js
@@ -48,7 +48,7 @@ export const fetchShortestPath = createAsyncThunk(
     async ({ network, startNodeId, endNodeId }, { rejectWithValue }) => {
       try {
         console.log(`Fetching shortest path with network: ${network}, ${startNodeId}, ${endNodeId}`);
-        const response = await fetch(`/api/shortest_path/${network}/${startNodeId}/${endNodeId}/`);
+        const response = await fetch(`https://dpolejni.pythonanywhere.com/api/shortest_path/${network}/${startNodeId}/${endNodeId}/`);
         if (!response.ok) throw new Error(`Network response was not ok: ${response.statusText}`);
         const data = await response.json();
         return data.path;
